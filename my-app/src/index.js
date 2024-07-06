b7d7f546d7490bb1c77fd652c271574d1fd28f63
전지웅 <wjswldnd2000@naver.com>
2024-07-06 18:37:19 +0900

    gitignore 재설정

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,14 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Clock from './chapter_04/Clock';
-/*
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-);
-*/
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 setInterval(() => {
   root.render(
