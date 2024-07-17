92b0546218f4131f7a97e7e2d0fd49c36162f049
전지웅 <wjswldnd2000@naver.com>
2024-07-17 20:07:39 +0900

    Component 합성과 상속

---
+++ my-app/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,14 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return (
+        <Card
+            title="타이틀"
+            BGColor="#00ffff">
+                <p>children01</p>
+                <p>children02</p>
+            </Card>
+    );
+}
+
+export default ProfileCard;
