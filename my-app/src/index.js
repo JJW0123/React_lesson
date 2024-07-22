0ab92e98358ad9a591598d01fabc1420a820fd6f
전지웅 <wjswldnd2000@naver.com>
2024-07-22 18:35:36 +0900

    styled-components 예제

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import MainPage from './chapter_15/MainPage';
+import Blocks from './chapter_16/Blocks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <MainPage/>
+    <Blocks/>
   </React.StrictMode>
 );
 
